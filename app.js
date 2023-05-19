5690d67ff3104d64f330eb4a44175ffa34c1591e
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-19 17:03:16 -0300

    se importan funciones mostrarMenu y pausa

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 require('colors');
+
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
 console.clear();
 
 const main = () => {
     console.log('Hola Mundo')
+
+    let opt = '';
+
+    mostrarMenu();
+    // do {
+    //     mostrarMenu();
+    // } while (opt !== 0)
+
+    // pausa();
 }
 
 main();
