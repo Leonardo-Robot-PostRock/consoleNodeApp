43af5cc059fb67a0009932775ef0d892b6748b4d
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-23 17:31:57 -0300

    se importa leerInput, se crea tarea y se muestra listado de tareas

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('colors');
 
-const { inquirerMenu, pausa, confirmacion } = require('./helpers/inquirer');
+const { inquirerMenu, pausa, confirmacion, leerInput } = require('./helpers/inquirer');
 const Tareas = require('./models/tareas');
 
 console.clear();
@@ -10,13 +10,25 @@ const main = async () => {
 
     let opt = '';
     let confirmar = '';
-
+    const tareas = new Tareas();
 
     do {
         // se espera respuesta de inquirer Menu y luego se guarda la salida de la función en opt.
         opt = await inquirerMenu();
         //si opt no es igual '0' entonces, se espera respuesta de pausa();
-        console.log({ opt });
+
+        switch (opt) {
+            case '1':
+                //crear opcion               
+                const desc = await leerInput('Descripción: ');
+                tareas.crearTarea(desc);
+                break;
+            case '2':
+                console.log(tareas._listado);
+
+            default:
+                break;
+        }
 
         if (opt !== '0') {
             await pausa();
