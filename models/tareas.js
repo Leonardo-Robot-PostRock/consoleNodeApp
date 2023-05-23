de1270b83f5f8161a4f72d104f1b0043c1059400
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-23 17:39:26 -0300

    se crea Metodo para crear Tarea se guarda con nombre de id el objeto

--- models/tareas.js
+++ models/tareas.js
@@ -3,10 +3,17 @@
  *      { 'uuid:12343534-41232123-2': { id:12, desc:asd, completadoEn:92231 } }
  */
 
+const Tarea = require("./tarea");
+
 class Tareas {
     _listado = {};
     constructor() {
         this._listado = {};
+    }
+
+    crearTarea(desc = '') {
+        const tarea = new Tarea(desc);
+        this._listado[tarea.id] = tarea;
     }
 }
 
