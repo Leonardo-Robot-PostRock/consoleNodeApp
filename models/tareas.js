d28540c992279cb6c8784ca069e9465ecc544af8
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-23 16:51:32 -0300

    se crea clase Tareas, inicializa la instancia _listado a un objeto vacio; afuera y dentro del constructor

---
+++ models/tareas.js
@@ -0,0 +1,13 @@
+/**
+ * _listado:
+ *      { 'uuid:12343534-41232123-2': { id:12, desc:asd, completadoEn:92231 } }
+ */
+
+class Tareas {
+    _listado = {};
+    constructor() {
+        this._listado = {};
+    }
+}
+
+module.exports = Tareas;
