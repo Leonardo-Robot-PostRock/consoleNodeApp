afe2a4607817046168ddd511397d8dfd11dc29b2
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-23 17:35:06 -0300

    se crea funcion leerInput, se crea pregunta de tipo input

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -87,8 +87,28 @@ const confirmacion = async () => {
     return confirmar;
 }
 
+const leerInput = async (message) => {
+    const question = [
+        {
+            type: 'input',
+            name: 'desc',
+            message,
+            validate(value) {
+                if (value.length === 0) {
+                    return 'Por favor ingrese un valor';
+                }
+                return true;
+            }
+        }
+    ];
+
+    const { desc } = await inquirer.prompt(question);
+    return desc;
+}
+
 module.exports = {
     inquirerMenu,
     pausa,
-    confirmacion
+    confirmacion,
+    leerInput
 }
