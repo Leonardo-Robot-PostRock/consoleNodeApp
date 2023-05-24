d669b8579bb0a3fe1f7ff9fa1b32e625be8b9d6d
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-24 04:22:39 -0300

    se llama metodo listadoArr

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const main = async () => {
                 tareas.crearTarea(desc);
                 break;
             case '2':
-                console.log(tareas._listado);
+                console.log(tareas.listadoArr);
 
             default:
                 break;
