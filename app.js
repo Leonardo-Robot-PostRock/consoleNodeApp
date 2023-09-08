b0ce577edc1ae88cd7894a996fc70b66fddb0eb5
Leonardo <leonardo38361@gmail.com>
2023-09-07 22:15:03 -0300

    identación, se recibe objeto en saveDB

--- app.js
+++ app.js
@@ -1,42 +1,49 @@
 require('colors');
 
-const { inquirerMenu, pausa, confirmacion, leerInput } = require('./helpers/inquirer');
+const {
+	inquirerMenu,
+	pausa,
+	confirmacion,
+	leerInput,
+} = require('./helpers/inquirer');
+const { saveDB } = require('./helpers/saveFile');
 const Tareas = require('./models/tareas');
 
 console.clear();
 
 const main = async () => {
+	let opt = '';
+	let confirmar = '';
+	const tareas = new Tareas();
 
-    let opt = '';
-    let confirmar = '';
-    const tareas = new Tareas();
+	do {
+		// se espera respuesta de inquirer Menu y luego se guarda la salida de la función en opt.
+		opt = await inquirerMenu();
 
-    do {
-        // se espera respuesta de inquirer Menu y luego se guarda la salida de la función en opt.
-        opt = await inquirerMenu();
+		switch (opt) {
+			case '1':
+				//crear opcion
+				const desc = await leerInput('Descripción: ');
+				tareas.crearTarea(desc);
+				break;
+			case '2':
+				console.log(tareas.listadoArr);
 
-        switch (opt) {
-            case '1':
-                //crear opcion               
-                const desc = await leerInput('Descripción: ');
-                tareas.crearTarea(desc);
-                break;
-            case '2':
-                console.log(tareas.listadoArr);
+			default:
+				break;
+		}
 
-            default:
-                break;
-        }
+		saveDB(tareas.listadoArr);
 
-        //si opt no es igual '0' entonces, se espera respuesta de pausa();
-        if (opt !== '0') {
-            await pausa();
-        }
-        console.clear();
-        if (opt === '0') {
-            confirmar = await confirmacion();
-        };
-    } while (opt !== '0' || confirmar === 'no');
-}
+		//si opt no es igual '0' entonces, se espera respuesta de pausa();
+		if (opt !== '0') {
+			await pausa();
+		}
+		console.clear();
+		if (opt === '0') {
+			confirmar = await confirmacion();
+		}
+	} while (opt !== '0' || confirmar === 'no');
+};
 
 main();
