3aeb831d36f0fa199da36363b1b44e05a82fb662
Leonardo <leonardo38361@gmail.com>
2023-09-08 02:02:19 -0300

    se cargan las tareas

--- models/tareas.js
+++ models/tareas.js
@@ -3,31 +3,37 @@
  *      { 'uuid:12343534-41232123-2': { id:12, desc:asd, completadoEn:92231 } }
  */
 
-const Tarea = require("./tarea");
+const Tarea = require('./tarea');
 
 class Tareas {
-    _listado = {};
+	_listado = {};
 
-    get listadoArr() {
-        const listado = [];
+	get listadoArr() {
+		const listado = [];
 
-        // return Object.values(this._listado) Esto es lo mismo;
-        Object.keys(this._listado).forEach(key => {
-            const tarea = this._listado[key];
-            listado.push(tarea);
-        })
+		// return Object.values(this._listado) Esto es lo mismo;
+		Object.keys(this._listado).forEach((key) => {
+			const tarea = this._listado[key];
+			listado.push(tarea);
+		});
 
-        return listado;
-    }
+		return listado;
+	}
 
-    constructor() {
-        this._listado = {};
-    }
+	constructor() {
+		this._listado = {};
+	}
 
-    crearTarea(desc = '') {
-        const tarea = new Tarea(desc);
-        this._listado[tarea.id] = tarea;
-    }
+	cargarTareasFromArr = (tareas = []) => {
+		tareas.forEach((tarea) => {
+			this._listado[tarea.id] = tarea;
+		});
+	};
+
+	crearTarea(desc = '') {
+		const tarea = new Tarea(desc);
+		this._listado[tarea.id] = tarea;
+	}
 }
 
 module.exports = Tareas;
